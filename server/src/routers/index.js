e1e9330a1d9e92721e2fe714a11e20fa7f2cd5df
krisikostadinov <krisi.199898@gmail.com>
2023-05-06 19:05:19 +0300

    delete others

--- server/src/routers/index.js
+++
@@ -1,9 +0,0 @@
-const commonRouter = require("@src/routers/common");
-const usersRouter = require("@src/routers/users");
-const passwordsRouter = require("@src/routers/passwords");
-
-module.exports = {
-    commonRouter,
-    usersRouter,
-    passwordsRouter,
-}
