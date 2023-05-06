e95165107d540f4c89f839e4d0a903cc940e9018
krisikostadinov <krisi.199898@gmail.com>
2023-05-06 18:56:45 +0300

    feature: login is implemented

--- client/src/components/auth/login/LoginView.vue
+++ client/src/components/auth/login/LoginView.vue
@@ -1,9 +1,32 @@
 <template>
-  <simple-dialog  @close="handleClose">
+  <simple-dialog @close="handleClose">
     <template v-slot:header>Login</template>
     <template v-slot:body>
-      <form class="px-5">
-        Body
+      <form @submit.prevent="handleLogin" class="px-5">
+        <p v-if="user.error" class="mb-5 text-red-500 text-center">
+          {{ user.error }}
+        </p>
+        <div class="mb-5">
+          <label for="email">Имейл Адрес</label>
+          <input
+            type="email"
+            placeholder="Вашия имейл адрес..."
+            id="email"
+            class="w-full py-2 px-4 mt-2 rounded border"
+            v-model="user.credentials.email"
+          />
+        </div>
+        <div class="mb-5">
+          <label for="password">Парола</label>
+          <input
+            type="password"
+            placeholder="Вашата парола..."
+            id="password"
+            class="w-full py-2 px-4 mt-2 rounded border"
+            v-model="user.credentials.password"
+          />
+        </div>
+        <button type="submit" class="button">Вход в системата</button>
       </form>
     </template>
   </simple-dialog>
@@ -12,6 +35,7 @@
 <script>
 // stores
 import { useEnvStore } from "@src/stores/env";
+import { useUserStore } from "@src/stores/user";
 
 // dialogs
 import SimpleDialog from "@src/components/dialogs/SimpleDialog.vue";
@@ -24,13 +48,17 @@ export default {
   },
   setup() {
     const env = useEnvStore();
+    const user = useUserStore();
     const functions = {
+      handleLogin: () => {
+        user.login();
+      },
       handleClose: () => {
         env.dialogs.auth.login = false;
-      }
-    }
-    return { ...functions }
-  }
+      },
+    };
+    return { user, ...functions };
+  },
 };
 </script>
 
