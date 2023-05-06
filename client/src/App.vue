982623f0c9881c6e7028d1b8de9c2db343f14b56
krisikostadinov <krisi.199898@gmail.com>
2023-05-06 09:46:51 +0300

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pt-5">
+    <h1 class="text-4xl text-center">Мениджър на пароли</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppView",
+};
+</script>
